236537a302749f42da11f9e615c8814d915f237e
MD.Rakibul Islam <rakibprodan1395@gmail.com>
2025-02-22 00:04:39 +0600

    Signup Page Created

---
+++ src/components/pages/Signup.js
@@ -0,0 +1,36 @@
+import classes from "../../styles/Signup.module.css";
+import Button from "../button/Button";
+import Checkbox from "../form/Checkbox";
+import Form from "../form/Form";
+import TextInput from "../form/TextInput";
+import Illustration from "../illustration/Illustration";
+
+export default function Signup() {
+  return (
+    <>
+      <h1>Create an account</h1>
+      <div className="column">
+        <Illustration />
+        <Form className={`${classes.signup}`}>
+          <TextInput type="text" placeholder="Enter name" icon="person" />
+          <TextInput
+            type="text"
+            placeholder="Enter email"
+            icon="alternate_email"
+          />
+          <TextInput type="text" placeholder="Enter password" icon="lock" />
+          <TextInput
+            type="text"
+            placeholder="Confirm password"
+            icon="lock_clock"
+          />
+          <Checkbox type="checkbox" text="I agree to the Terms & Conditions" />
+          <Button>Submit now</Button>
+          <div class="info">
+            Already have an account? <a href="login.html">Login</a> instead.
+          </div>
+        </Form>
+      </div>
+    </>
+  );
+}
--- src/components/videos/Videos.js
+++ src/components/videos/Videos.js
@@ -5,6 +5,12 @@ export default function Videos() {
   return (
     <div className={classes.videos}>
       <Video />
+      <Video />
+      <Video />
+      <Video />
+      <Video />
+      <Video />
+      <Video />
     </div>
   );
 }
